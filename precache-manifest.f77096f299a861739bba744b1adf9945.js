b2cf172ac47b8be28e95eccee261762a1bff53ba
Koassi AKAKPO <akfred2007@outlook.com>
2018-11-26 21:17:02 +0000

    Updates

---
+++ precache-manifest.f77096f299a861739bba744b1adf9945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b1a27ca8cc684cea5b9c28f179b09f6",
+    "url": "/github.io/react-complete-crud-todo-app/static/media/loading.1b1a27ca.gif"
+  },
+  {
+    "revision": "c96b52d250acb22aba78",
+    "url": "/github.io/react-complete-crud-todo-app/static/js/runtime~main.c96b52d2.js"
+  },
+  {
+    "revision": "8ae16a00f8d614d4a122",
+    "url": "/github.io/react-complete-crud-todo-app/static/js/main.8ae16a00.chunk.js"
+  },
+  {
+    "revision": "a915e5937dc83289d170",
+    "url": "/github.io/react-complete-crud-todo-app/static/js/1.a915e593.chunk.js"
+  },
+  {
+    "revision": "8ae16a00f8d614d4a122",
+    "url": "/github.io/react-complete-crud-todo-app/static/css/main.3f83a73d.chunk.css"
+  },
+  {
+    "revision": "df3754389f0e0b5bc66ebb44a04afd8b",
+    "url": "/github.io/react-complete-crud-todo-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github.com.io/react-complete-crud-todo-app/precache-manifest.f88e802b69f1479b2ded6d71604d7caa.js"
+  "/github.io/react-complete-crud-todo-app/precache-manifest.f77096f299a861739bba744b1adf9945.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/github.com.io/react-complete-crud-todo-app/index.html", {
+workbox.routing.registerNavigationRoute("/github.io/react-complete-crud-todo-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
