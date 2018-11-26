5276e19cc3b1f4b536de4b05b569708e2f38d923
Koassi AKAKPO <akfred2007@outlook.com>
2018-11-26 21:11:55 +0000

    Updates

---
+++ precache-manifest.9d6215d6d366f1179bd1e0ed2d8c2b43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b1a27ca8cc684cea5b9c28f179b09f6",
+    "url": "%20//akfred/github.com.io/react-complete-crud-todo-app/static/media/loading.1b1a27ca.gif"
+  },
+  {
+    "revision": "2776ba2844d5488a928e",
+    "url": "%20//akfred/github.com.io/react-complete-crud-todo-app/static/js/runtime~main.2776ba28.js"
+  },
+  {
+    "revision": "dc68c8c2043376aac3f2",
+    "url": "%20//akfred/github.com.io/react-complete-crud-todo-app/static/js/main.dc68c8c2.chunk.js"
+  },
+  {
+    "revision": "a915e5937dc83289d170",
+    "url": "%20//akfred/github.com.io/react-complete-crud-todo-app/static/js/1.a915e593.chunk.js"
+  },
+  {
+    "revision": "dc68c8c2043376aac3f2",
+    "url": "%20//akfred/github.com.io/react-complete-crud-todo-app/static/css/main.3f83a73d.chunk.css"
+  },
+  {
+    "revision": "b5139c916170e5c5f6f981b3042dbf69",
+    "url": "%20//akfred/github.com.io/react-complete-crud-todo-app/index.html"
+  }
+];
